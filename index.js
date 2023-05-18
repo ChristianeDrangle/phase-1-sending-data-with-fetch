b66cf3c3a4ea77005b4233a0801e4f468adece13
Christiane Drangle <positivitee@proton.me>
2023-05-18 17:58:55 -0400

    Sending Data with Fetch passing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+  const submitData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  }
+
+  return fetch("http://localhost:3000/users", submitData)
+  .then((response) => response.json()) 
+  .then ((person) => {
+    document.body.textContent=person.id
+  })
+  .catch((error) => {document.body.textContent=error.message})
+  }
+  
+submitData("Monica", "Wills@test.com")
